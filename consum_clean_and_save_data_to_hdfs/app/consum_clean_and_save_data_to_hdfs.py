a77234d8ca8afb16a73416aac52c1a4469e41f1b
Mahdi <155837868+mahdimlo@users.noreply.github.com>
2024-08-06 11:41:08 +0330

    latest

---
+++ consum_clean_and_save_data_to_hdfs/app/consum_clean_and_save_data_to_hdfs.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from kafka import KafkaConsumer
+import json
+import set_log
+from hdfs import InsecureClient
+
+
+
+def consum_data_from_kafka():
+    try:
+        consumer = KafkaConsumer("boors_data_daily", 
+                                    bootstrap_servers=['kafka-broker:29092'],
+                                    auto_offset_reset='earliest',
+                                    group_id='consum_boors_data_daily',
+                                    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+                                    security_protocol= 'PLAINTEXT'
+                                    )
+        all_data = []
+        for massage in consumer:
+            data = massage.value
+            all_data.append(data)
+        return all_data
+        set_log.info_logger.info(f"All data was properly consumed from Kafka")
+    except Exception as e:
+        set_log.error_logger.error(f"Unfortunately, we could not consum the data from Kafka: {str(e)}")
+
+
+def clean_data(all_data):
+    try:
+        df = pd.DataFrame(all_data)
+        if df.shape[0] < 2:
+            set_log.info_logger.info("This data is empty and was removed")
+        else:
+            return df
+            set_log.info_logger.info("The data was cleared")
+    except Exception as e:
+        set_log.error_logger.error(f"There is a problem in clearing the data: {str(e)}")
+
+
+def save_data_to_hdfs(df, hdfs_path):
+    try:
+        client = InsecureClient('hdfs://namenode:50070', user='hadoop')
+        with client.write(hdfs_path, encoding='utf-8', overwrite=True) as writer:
+            df.to_csv(writer)
+        set_log.info_logger.info(f"Data successfully saved to HDFS at {hdfs_path}")
+    except Exception as e:
+        set_log.error_logger.error(f"Error saving data to HDFS: {str(e)}")
+
+
+def main():
+    all_data = consum_data_from_kafka()
+    if all_data is not None:
+        df = clean_data(all_data)
+        date_str = df['تاریخ'].iloc[0]
+        hdfs_path = f"/user/boors_data_csv/{str(date_str)}.csv"
+        save_data_to_hdfs(df, hdfs_path)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ get_and_produce_data_to_kafka/app/get_and_produce_data_to_kafka.py
@@ -0,0 +1,48 @@
+import requests
+from jdatetime import datetime as jdatetime
+from jdatetime import timedelta
+import pandas as pd
+from kafka import KafkaProducer
+import set_log
+import json
+
+
+def produce_data_to_kafka(response, date):
+    try: 
+        df = pd.read_excel(response.content)
+        df['تاریخ'] = date
+        data = df.to_dict(orient='records')
+        producer = KafkaProducer(
+            bootstrap_servers=['kafka-broker:29092'],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+        for record in data:
+            producer.send('boors_data_daily', record)
+        set_log.info_logger.info(f"The data for this date {date} was correctly sent to Kafka")
+    except Exception as e:
+        set_log.error_logger.error(f"Error sending data to Kafka: {str(e)}")
+
+
+def get_data():
+    try:
+        now = jdatetime.now().date()
+        now -= timedelta(days=1)
+        if now.weekday() < 5:
+            url = f"https://members.tsetmc.com/tsev2/excel/MarketWatchPlus.aspx?d={str(now)}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                set_log.info_logger.info(f"We have successfully connected to this URL: {url}")
+                produce_data_to_kafka(response, now)
+            else:
+                set_log.error_logger.error(f"Could not connect to this URL: {url}")
+        else:
+            set_log.info_logger.info(f"{str(now)}: This day is weekend")
+    except Exception as e:
+        set_log.error_logger.error(f"Error downloading files: {str(e)}")
+
+
+def main():
+    get_data()
+
+if __name__ == '__main__':
+    main()
